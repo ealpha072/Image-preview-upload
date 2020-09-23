8d5e64ef7950999981f3c91c50ec48fefcad4d55
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-23 23:31:31 +0300

    Update server.php

--- server.php
+++ server.php
@@ -1,9 +1,23 @@
-<?php 
+<?php
+    $msg = '';
+    $msg_class = ''; 
     $conn = mysqli_connect('localhost','root','','img-upload');
     //if button is clicked;
     if(isset($_POST['save-profile'])){
         $userbio = mysqli_real_escape_string($conn,$_POST['bio']);
-        $imageNmae = time(). '-'. $_FILES['profile-img']['name'];
+        $imageName = time(). '-'. $_FILES['profile-img']['name'];
+        $location = "images/"
+        $targeted_file = $location.basename($imageName);
+        //validation(img size;size is in bytes);
+        if($_FILES['profile-img']['size']>200000){
+            $msg = "Image size too big.Max size is 200kb";
+            $msg_class = "alert-danger";
+        }
+        //check if file exists
+        if(file_exists($targeted_file)){
+            $msg  = 'Image already exists';
+            $msg_class = 'alert-danger';
+        }
     }
 
 
