f727762bec688557b835fe27e317d108c8c98d09
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-24 11:45:21 +0300

    Add files via upload

---
+++ allprofiles.php
@@ -0,0 +1,43 @@
+<?php 
+    $conn = mysqli_connect('localhost','root','','img-upload');
+    $results = mysqli_query($conn, "SELECT * FROM users");
+    $allusers = mysqli_fetch_assoc($results,MYSQLI_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Admin-User System</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <a href="myform.php">New Profile</a>
+            <br>
+            <br>
+            <table class="table table-bordered">
+                <thead>
+                    <th>Image</th>
+                    <th>Bio</th>
+                </thead>
+                <tbody>
+                    <?php foreach ($allusers as $user): ?>
+                        <tr>
+                            <td><img src=""></td>
+                        </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
